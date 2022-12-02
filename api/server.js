a51c073970b6f47f1c8ff34ff7220a1cb569168a
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-12-02 10:00:53 -0800

    set up server listening port

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
-// build your server here and require it from index.js
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: `Path ${req.path} not found`
+    });
+});
+
+module.exports = server;
